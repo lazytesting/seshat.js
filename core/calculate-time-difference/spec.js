848cb2f9536aaea408133f4cf659462717eea85e
Maarten Groeneweg <mgroeneweg@mirabeau.nl>
2016-10-04 08:35:59 +0200

    added travis config, removed unused test file

--- core/calculate-time-difference/spec.js
+++ core/calculate-time-difference/spec.js
@@ -4,7 +4,7 @@ const time = require('./app.js');
 describe('calculate time difference', () => {
     it('should calculate 1 minute as 60 seconds', () => {
         var pos1 = {date: "2015-01-11T15:10:11.000+01:00"};
-        var pos2 = {date: "2015-01-11T15:10:12.000+01:00"};
+        var pos2 = {date: "2015-01-11T15:11:11.000+01:00"};
         var result = time.Get(pos1,pos2);
         assert(result == 60);
     });
