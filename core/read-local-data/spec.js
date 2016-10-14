28fb8dcf8693dfc7b2474fa8f39c8338313f3d6b
Maarten Groeneweg <mgroeneweg@mirabeau.nl>
2016-10-14 11:28:02 +0200

    changed path for read-local-data testdata

--- core/read-local-data/spec.js
+++ core/read-local-data/spec.js
@@ -3,7 +3,7 @@ const data = require('./app.js');
 
 describe('Read and parse data from disk', () => {
     it('should return results', () => {
-        var path = "./test/data.gpx"
+        var path = __dirname + "/test/data.gpx"
         assert(data.Get(path) != null);
     });
 });
