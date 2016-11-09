38d37849160b0da641d4a0faee01727972b752ae
LazyTesting <mgroeneweg@mirabeaunl>
2016-11-09 20:22:31 +0100

    fixed eslint errors, direct exports instead of Get

--- core/map-two/app.js
+++ core/map-two/app.js
@@ -1,10 +1,10 @@
-module.exports = {
-    Get: function(array, callback) {
-        const results = [];
-        for (let i = 1; i <= array.length - 1; i++) {
-            results.push(callback(array[i], array[i - 1]));
-        }
-
-        return results;
+const get = function(array, callback) {
+    const results = [];
+    for (let i = 1; i <= array.length - 1; i++) {
+        results.push(callback(array[i], array[i - 1]));
     }
+
+    return results;
 };
+
+module.exports = get;
--- core/map-two/spec.js
+++ core/map-two/spec.js
@@ -3,11 +3,11 @@ const maptwo = require('./app.js');
 
 describe('map each value with the next one in the list', () => {
     it('simple array', () => {
-        const testfunc = function(a, b) {
+        const testFunction = function(a, b) {
             return a + b;
         };
         const list = [1, 2, 5];
-        const result = maptwo.Get(list, testfunc);
+        const result = maptwo(list, testFunction);
         const expected = [3, 7];
         assert.deepEqual(result, expected);
     });
