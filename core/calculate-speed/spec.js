4672b95194ad989f8e682c9f241f3ae0535a6f4a
LazyTesting <mgroeneweg@mirabeaunl>
2016-11-10 15:19:39 +0100

    fixed codeduplication in tests

--- core/calculate-speed/spec.js
+++ core/calculate-speed/spec.js
@@ -3,16 +3,12 @@ const speed = require('./app.js');
 
 describe('calculate speed', () => {
     it('should calculate km/h correctly', () => {
-        const distance = 10;
-        const time = 1;
-        const result = speed(distance, time);
+        const result = speed(10, 1);
         assert.deepEqual(result, 36);
     });
 
     it('should handle 0 distance', () => {
-        const distance = 0;
-        const time = 1;
-        const result = speed(distance, time);
+        const result = speed(0, 1);
         assert.deepEqual(result, 0);
     });
 });
