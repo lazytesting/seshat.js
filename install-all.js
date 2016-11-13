586b12bb28bcb7ef859f7c1f0380c9ebad033988
LazyTesting <mgroeneweg@mirabeaunl>
2016-11-13 18:42:01 +0100

    fixed typo in package json, added logging to install all script

--- install-all.js
+++ install-all.js
@@ -2,6 +2,7 @@ const glob = require('glob');
 const execSync = require('child_process').execSync;
 
 const install = function(path) {
+    console.log('installing dependencies for ', path)
     execSync('npm i', {
         cwd: path
     });
